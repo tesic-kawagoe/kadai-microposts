4960001f2171943211fc08db9e793f467cb5e4ed
tesic-kawagoe <con@tes-ic.com>
2018-08-01 04:37:24 +0000

    favorite model

--- app/Micropost.php
+++ app/Micropost.php
@@ -6,10 +6,45 @@ use Illuminate\Database\Eloquent\Model;
 
 class Micropost extends Model
 {
-    protected $fillable = ['content', 'user_id'];
+    protected $fillable = ['content', 'user_id', 'micropost_id'];
     
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+    
+    public function favorites()
+    {
+        return $this->belongsToMany(Micropost::class, 'micropost_favorite', 'user_id', 'micropost_id')->withTimestamps();
+    }
+    
+    public function favorite($micropostId)
+    {
+        $exist = $this->is_favorite($micropostId);
+        $its_micropost = $this->id == $micropostId;
+        
+        if ($exist || $its_micropost) {
+            return false;
+        } else {
+            $this->favorites()->attach($micropostId);
+            return true;
+        }
+    }
+    
+    public function unfavorite($micropostId)
+    {
+        $exist = $this->is_favorite($micropostId);
+        $its_micropost = $this->id == $micropostId;
+        
+        if ($exist && !$its_micropost) {
+            $this->favorites()->detach($micropostId);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public function is_favorite($micropostId) {
+        return $this->favorites()->where('micropost_id', $micropostId)->exists();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,15 +21,11 @@ Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
 Route::group(['middleware' => 'auth'], function () {
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
-     Route::group(['prefix' => 'users/{id}'], function () {
-        Route::post('follow', 'UserFollowController@store')->name('user.follow');
-        Route::delete('unfollow', 'UserFollowController@destroy')->name('user.unfollow');
-        Route::get('followings', 'UsersController@followings')->name('users.followings');
-        Route::get('followers', 'UsersController@followers')->name('users.followers');
-        Route::post('favorite', 'UserFavoriteController@store')->name('user.favorite');
-        Route::delete('unfavorite', 'UserFavoriteController@destroy')->name('user.unfavorite');
-        Route::get('favorites', 'UsersController@favorites')->name('users.favorites');
-    });
-    
+        Route::group(['prefix' => 'users/{id}'], function () {
+            Route::post('follow', 'UserFollowController@store')->name('user.follow');
+            Route::delete('unfollow', 'UserFollowController@destroy')->name('user.unfollow');
+            Route::get('followings', 'UsersController@followings')->name('users.followings');
+            Route::get('followers', 'UsersController@followers')->name('users.followers');
+        });
     Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
